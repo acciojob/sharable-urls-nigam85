ccce7076ac01a7a795298a9391aa9670af6c762c
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-09-08 11:31:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // your code here
+ document.getElementById('urlForm').addEventListener('submit', function (e) {
+            e.preventDefault();
+
+            const name = document.getElementById('name').value;
+            const year = document.getElementById('year').value;
+
+            const queryString = `?name=${encodeURIComponent(name)}&year=${year}`;
+            const url = `https://localhost:8080/${queryString}`;
+
+            document.getElementById('url').textContent = url;
+        });
