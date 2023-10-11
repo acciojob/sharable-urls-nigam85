941d614ff01e5cbc95e7a2f7163627b13a72cfc6
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-10-11 10:16:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-// your code here
- document.getElementById('urlForm').addEventListener('submit', function (e) {
-            e.preventDefault();
+// // your code here
+//  document.getElementById('urlForm').addEventListener('submit', function (e) {
+//             e.preventDefault();
 
-            const name = document.getElementById('name').value;
-            const year = document.getElementById('year').value;
+//             const name = document.getElementById('name').value;
+//             const year = document.getElementById('year').value;
 
-            const queryString = `?name=${encodeURIComponent(name)}&year=${year}`;
-            const url = `https://localhost:8080/${queryString}`;
+//             const queryString = `?name=${encodeURIComponent(name)}&year=${year}`;
+//             const url = `https://localhost:8080/${queryString}`;
 
-            document.getElementById('url').textContent = url;
-        });
+//             document.getElementById('url').textContent = url;
+//         });
+
+ function generateURL() {
+      const name = document.getElementById('name').value;
+      const year = document.getElementById('year').value;
+      const baseUrl = "https://localhost:8080/";
+      
+      // Create a query string with the provided values
+      let queryString = "?";
+      if (name) {
+        queryString += "name=" + encodeURIComponent(name);
+      }
+      if (year) {
+        if (name) {
+          queryString += "&";
+        }
+        queryString += "year=" + year;
+      }
+      
+      // Update the h3 element with the generated URL
+      document.getElementById('url').textContent = baseUrl + queryString;
+    }
